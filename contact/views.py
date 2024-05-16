47a31c86c24decdf0832e73fd6559190a3f5522c
ekinf <ekinfiliza@hotmail.com>
2024-05-16 16:12:06 +0200

    feat: training added

---
+++ contact/views.py
@@ -0,0 +1,32 @@
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact_form(request):
+    print("hello from contact_from function")
+    if request.method == 'POST':
+        nameText = request.POST['name']
+        emailText = request.POST['email']
+        subjectText = request.POST['subject']
+        messageText = request.POST['message']
+        print("hello from contact_from if state")
+
+        send_mail(
+            "Message From :" + subjectText,
+            f"Name: {nameText}\nEmail: {emailText}\n\n{messageText}",  # E-posta gövdesi
+            'your-email@example.com',  # Gönderen e-posta adresi
+            ['ekinfilizatass@gmail.com'],  # Alıcı e-posta adresi
+        )
+
+        print("if worked")
+        return render(request, 'index.html', {"message_name": nameText})
+    else:
+
+        print("else worked")
+        return render(request, 'index.html', {"message_name": "sa"})
